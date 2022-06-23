e14ddb1a661e56f87c48e18577cc363d54b2fa69
Francisco J Nogueira <fjosenogueira@gmail.com>
2022-06-23 14:31:41 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if (!user) {
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro) {
+    if (user.todos.length === 10) {
+      return response.status(403).json();
+    }
+  }
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { idToDo } = request.params;
+
+  const uuidIsValid = uuidv4().validate(idToDo);
+
+  const user = users.find(u => u.username === username);
+
+  if (!user) {
+    return response.status(404).json();
+  }
+
+  if (!uuidIsValid) {
+    return response.status(400).json();
+  }
+
+  const todo = user.find(utd => utd.id === id);
+
+  if (!todo) {
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  const user = users.find(u => u.id === id);
+
+  if(!user) {
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
